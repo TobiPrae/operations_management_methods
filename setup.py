e0f636d87ab8b90c5224c9ceca6f3e852851ad44
TobiPrae <50541402+TobiPrae@users.noreply.github.com>
2020-12-19 17:38:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='operations_management_methods',
-      version='0.1',
+      version='0.5',
       description='Methods used for OM',
       packages=['operations_management_methods'],
       zip_safe=False)
